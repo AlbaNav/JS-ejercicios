3341a0473f921628f22faa1f7bb949bf22a0cce5
Alba <alba.m.navarro@gmail.com>
2021-12-09 20:18:04 +0100

    iteracion2 actualizada

---
+++ upgrade-basic-algorithms/iteracion_3/iteracion3.js
@@ -0,0 +1,28 @@
+//1.1//
+
+let sum=10*5;
+console.log(sum);
+
+//1.2//
+
+let div=10/2;
+console.log(div);
+
+//1.3//
+
+let rest=15%9;
+console.console.log(res);
+
+//1.4//
+
+const y=10;
+const z=5;
+let result=y+z;
+console.log(result);
+
+//1.5//
+
+const a=10;
+const b=5;
+let resu=a*b;
+console.log(resu);
