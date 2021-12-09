6d4fbf6952c44ff162bd8e171e0d67fbd99b2a02
Alba <alba.m.navarro@gmail.com>
2021-12-09 19:57:25 +0100

    hasta iteracion3

---
+++ upgrade-basic-algorithms/iteracion_1/iteracion1.js
@@ -0,0 +1,7 @@
+//1.1//
+let myFavoriteHero="Hulk";
+let x =50;
+let h=5;
+let y=10;
+var z=h+y;
+console.log(z);
---
+++ upgrade-basic-algorithms/iteracion_2/iteracion2.js
@@ -0,0 +1,38 @@
+
+//1.1//
+
+const character ={
+    name:"Jack Sparrow",
+    age:25
+};
+
+//1.2//
+
+let firstName = "Jon";
+let lastName = "snow";
+const age = 24;
+
+console.log("Soy " + firstName + ' ' + lastName + ", tengo " + age + " años y me gustan los lobos.");
+
+//1.3//
+
+const toy1 ={
+    name: "Buss myYear",
+    price: 19
+};
+
+const toy2 ={
+    name: "Rallo mcKing",
+    price: 29
+};
+ 
+var suma= toy1.price + toy2.price;
+console.log(suma)
+
+//1.4//
+
+
+
+
+
+
