cc7f95bb567772492d77d20eb3a5687e8a889156
Alba <alba.m.navarro@gmail.com>
2021-12-11 11:56:03 +0100

    hasta iteracion6

---
+++ upgrade-basic-algorithms/iteracion_6/iteracion6.js
@@ -0,0 +1,22 @@
+//1.1//
+
+for (let i = 0; i < 10 ; i++) {
+    console.log(i);  
+}
+//1.2//
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0){
+    console.log(i);  
+}
+}
+//1.3//
+for (let i = 0; i < 10; i++) {
+    if (i<9) { 
+        console.log("intentando dormir")
+    }
+        else {
+            console.log("dormido")
+        }      
+    }
+    
